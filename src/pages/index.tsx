f5cb7defb51a87620f91d6006ed661619fc5b3bf
Franklin Martins <gentilezaxd@gmail.com>
2021-04-09 17:24:57 -0400

    Add server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
+import { GetServerSideProps } from 'next'
 
+interface IProduct {
+  id: string;
+  title: string;
+}
 
-export default function Home() {
+interface HomeProps{
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
-    <div>Hello Franklin</div>
+    <div>
+      <section>
+        <h1>Products</h1>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct =>{
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props:{
+      recommendedProducts
+    }
+  }
 }
